bbf8f9e5efa663b8fc7bd5a6cc885d8ef8cb3ad8
aarshvb <aarshvb@gmail.com>
2020-12-15 23:03:35 +0530

    change

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from 'react'
 import Amplify, { API, graphqlOperation } from 'aws-amplify'
 import { createTodo } from './graphql/mutations'
 import { listTodos } from './graphql/queries'
-
+import { withAuthenticator } from '@aws-amplify/ui-react'
 import awsExports from "./aws-exports";
 import Amplify from 'aws-amplify';
 import awsconfig from './aws-exports' ;
@@ -80,4 +80,4 @@ const styles = {
   button: { backgroundColor: 'black', color: 'white', outline: 'none', fontSize: 18, padding: '12px 0px' }
 }
 
-export default App
+export default withAuthenticator(App)
